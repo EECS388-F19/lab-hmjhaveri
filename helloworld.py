e3b5aed86bae61a15c5e9c5e0c3f594984a720dd
hmjhaveri <hjhaveri@umich.edu>
2019-09-09 13:36:38 -0400

    Finished lab assignment

---
+++ helloworld.py
@@ -0,0 +1,14 @@
+#Harsh Jhaveri - EECS 388 Lab 1
+#Imports
+from random import randint
+num1 = randint(1,100)
+num2 = randint(1,100)
+sum1 = num1 + num2
+avg1 = sum1/2.0
+
+#Print Statements
+print("Harsh Jhaveri")
+print("Num1: " + str(num1))
+print("Num2: " + str(num2))
+print("Sum: " + str(sum1))
+print("Average: " + str(avg1))
